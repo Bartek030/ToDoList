6743bd2c5401e74d3f4e3dc97deca97783e01ddd
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-09-26 22:55:55 +0200

    Initial commit

---
+++ src/main/java/pl_bartlomiej_swies/App.java
@@ -0,0 +1,10 @@
+package pl_bartlomiej_swies;
+
+import org.slf4j.LoggerFactory;
+
+public class App {
+    public static void main(String[] args) {
+        var logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello world");
+    }
+}
