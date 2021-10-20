e7bab19a566ee8da9760568790ab64fa587c797f
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-10-20 21:48:36 +0200

    Connecting to localhost

--- src/main/java/pl_bartlomiej_swies/ServletTest.java
+++ src/main/java/pl_bartlomiej_swies/ServletTest.java
@@ -1,15 +1,23 @@
 package pl_bartlomiej_swies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class ServletTest extends HttpServlet {
+
+    private final Logger logger = LoggerFactory.getLogger(ServletTest.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request got");
         resp.getWriter().write("Hello world!");
     }
 }
