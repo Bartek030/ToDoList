68dfa421ccd76f8143ed8f49bdb563160b5e9837
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-11-01 21:47:53 +0100

    TodoRepository and servlet created

---
+++ src/main/java/pl_bartlomiej_swies/todo/TodoRepository.java
@@ -0,0 +1,18 @@
+package pl_bartlomiej_swies.todo;
+
+import pl_bartlomiej_swies.HibernateUtil;
+
+import java.util.List;
+
+class TodoRepository {
+    List<Todo> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
