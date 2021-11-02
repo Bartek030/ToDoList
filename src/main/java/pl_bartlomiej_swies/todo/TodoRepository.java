a45ba9b4a7e8d34fea204199926575a84bb55be0
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-11-02 20:17:11 +0100

    Toggling tasks

--- src/main/java/pl_bartlomiej_swies/todo/TodoRepository.java
+++ src/main/java/pl_bartlomiej_swies/todo/TodoRepository.java
@@ -1,7 +1,6 @@
 package pl_bartlomiej_swies.todo;
 
 import pl_bartlomiej_swies.HibernateUtil;
-
 import java.util.List;
 
 class TodoRepository {
@@ -10,6 +9,18 @@ class TodoRepository {
         var transaction = session.beginTransaction();
 
         var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    Todo toggleTodo(Integer id) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.get(Todo.class, id);
+        result.setDone(!result.isDone());
 
         transaction.commit();
         session.close();
